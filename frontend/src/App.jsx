3648e63794c8cb380ebe5cd73640f07788c04da2
lukepattersn <ltp5305@gmail.com>
2025-05-05 13:18:05 -0500

    Clean up main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 // Import page components
 import Home from "./pages/Home";
-// import About from "./pages/About";
+import About from "./pages/About";
 import Menu from "./pages/Menu";
 import Catering from "./pages/Catering";
 import Checkout from "./pages/Checkout";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
-// import Team from "./pages/Team";
-// import Admin from "./pages/Admin";
+import Team from "./pages/Team";
+import Admin from "./pages/Admin";
 
 // Import cart components
 import { CartProvider } from "./components/cart/CartContext";
@@ -31,25 +31,30 @@ const App = () => {
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
-            {/* <Route path="/about" element={<About />} /> */}
+            <Route path="/about" element={<About />} />
             <Route path="/catering" element={<Catering />} />
             <Route path="/menu" element={<Menu />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/checkout" element={
-              <PrivateRoute>
-                <Checkout />
-              </PrivateRoute>
-            } />
-            <Route path="/profile" element={
-              <PrivateRoute>
-                <Profile />
-              </PrivateRoute>
-            } />
-            {/* <Route path="/team" element={<Team />} /> */}
-            {/* <Route path="/admin" element={<Admin />} /> */}
+            <Route
+              path="/checkout"
+              element={
+                <PrivateRoute>
+                  <Checkout />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <PrivateRoute>
+                  <Profile />
+                </PrivateRoute>
+              }
+            />
+            <Route path="/team" element={<Team />} />
+            <Route path="/admin" element={<Admin />} />
 
-            {/* Redirect to home for unknown routes */}
             <Route path="*" element={<Home />} />
           </Routes>
 
--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,8 +1,8 @@
 // src/components/layout/Navbar.jsx
-import React from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
-import { useAuth } from '../auth/AuthContext';
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { useAuth } from "../auth/AuthContext";
 
 export default function AppNavbar() {
   const { currentUser, logout } = useAuth();
@@ -10,7 +10,7 @@ export default function AppNavbar() {
 
   const handleLogout = () => {
     logout();
-    navigate('/');
+    navigate("/");
   };
 
   return (
@@ -35,71 +35,49 @@ export default function AppNavbar() {
         <Navbar.Toggle aria-controls="main-navbar-nav" />
         <Navbar.Collapse id="main-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link
-              as={NavLink}
-              to="/"
-              end
-              className="nav-link"    
-            >
+            <Nav.Link as={NavLink} to="/" end className="nav-link">
               Home
             </Nav.Link>
-            <Nav.Link
-              as={NavLink}
-              to="/menu"
-              className="nav-link"
-            >
+            <Nav.Link as={NavLink} to="/menu" className="nav-link">
               Menu
             </Nav.Link>
-            <Nav.Link
-              as={NavLink}
-              to="/about"
-              className="nav-link"
-            >
+            <Nav.Link as={NavLink} to="/about" className="nav-link">
               About Us
             </Nav.Link>
-            <Nav.Link
-              as={NavLink}
-              to="/catering"
-              className="nav-link"
-            >
+            <Nav.Link as={NavLink} to="/catering" className="nav-link">
               Catering
             </Nav.Link>
-            <Nav.Link
-              as={NavLink}
-              to="/team"
-              className="nav-link"
-            >
+            <Nav.Link as={NavLink} to="/team" className="nav-link">
               Team
             </Nav.Link>
-            <Nav.Link
-              as={NavLink}
-              to="/future"
-              className="nav-link"
-            >
-              Future
-            </Nav.Link>
-            <Nav.Link
-              as={NavLink}
-              to="/admin"
-              className="nav-link"
-            >
+            <Nav.Link as={NavLink} to="/admin" className="nav-link">
               Admin
             </Nav.Link>
           </Nav>
-          
+
           {/* User Authentication Controls */}
           <Nav>
             {currentUser ? (
-              <NavDropdown 
+              <NavDropdown
                 title={
                   <span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-person-circle me-1" viewBox="0 0 16 16">
-                      <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-                      <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      className="bi bi-person-circle me-1"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                      <path
+                        fillRule="evenodd"
+                        d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+                      />
                     </svg>
                     {currentUser.username}
                   </span>
-                } 
+                }
                 id="user-dropdown"
                 align="end"
               >
