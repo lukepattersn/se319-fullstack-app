17ddbb35071ff2972b7d87293068d1bd5de223b1
Baccam, Jeremiah J <jjbaccam@iastate.edu>
2025-05-04 20:04:36 -0500

    updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,27 @@
+// src/App.jsx
+
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Home     from './pages/Home';
+import Catering from './pages/Catering';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Navbar />
+
+      {/* push page content below the navbar (adjust pt-20 if your navbar height changes) */}
+      <div className="pt-20">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/catering" element={<Catering />} />
+        </Routes>
+      </div>
+
+      {/* footer on every page */}
+      <Footer />
     </Router>
   );
 }
